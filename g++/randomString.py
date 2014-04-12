9b0f1f00bb250c1fed10d5c23333c6849ce1b368
chunderstruck <dchun529@gmail.com>
2014-04-12 00:24:38 -0700

    + ‘g++/randomString.py’

---
+++ g++/randomString.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 
+
+import string
+import random
+lst = [random.choice(string.ascii_letters + string.digits) for n in xrange(30)]
+str = "".join(lst)
+print str
+ocwbKCiuAJLRJgM1bWNV1TPSH0F2Lb
+
